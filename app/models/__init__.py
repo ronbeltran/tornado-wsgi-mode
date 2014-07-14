9dfba2146d9cc51c66f7f81a327c10c20474fadd
Ronnie Beltran <rbbeltran.09@gmail.com>
2014-07-14 19:03:39 +0800

    Expose User class to create_all()

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -5,6 +5,7 @@ engine = create_engine('sqlite:///dev.db', echo=False)
 Base = declarative_base()
 metadata = Base.metadata
 
+from app.models.users import User
 
 def create_all():
     metadata.create_all(engine)
